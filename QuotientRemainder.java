339b5ac3fc32e1049851d8d828dbfbf613f71c0b
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-07-16 14:33:58 +0530

    UC6 Added

---
+++ QuotientRemainder.java
@@ -0,0 +1,17 @@
+package com.fifthdayassignment.program;
+
+public class Quotient_RemainderUC6 {
+	public static void main(String[] args) {
+		
+		 int number=45;
+		 int divisible_number=4;
+		 int quotient=1;
+		 int remainder=1;
+		 // Quotient & Remainde
+		 quotient=number / divisible_number ;
+		 remainder=number % divisible_number ;
+
+	 System.out.println("Quotient value= "+quotient);
+	 System.out.println("Remainder value= "+remainder);
+	}
+	}
