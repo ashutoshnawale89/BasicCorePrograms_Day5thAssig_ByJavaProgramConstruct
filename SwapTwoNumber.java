160302a56b59a20e24c2dfe252f362a3cee8da0d
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-07-16 14:36:11 +0530

    UC7 Added

---
+++ SwapTwoNumber.java
@@ -0,0 +1,16 @@
+package com.fifthdayassignment.program;
+
+public class SwapTwoNumberUC7 {
+	 public static void main(String[] args) {
+		 // VALUES PUT ON VARIABLE1 & VARIABLE2
+			int Variable1=25000; // SMALL VALUE PUT
+		    int Variable2=100; // LARGE VALUE PUT
+		    int num=Variable1;
+		    
+		    Variable1=Variable2;
+		    Variable2=num;
+
+		    System.out.println("Variable1 =  "+Variable1 + "  &&&  " + "Variable2  = "+ Variable2);
+		  
+		  }
+		}
