6b2f0fc94522d71fff6f0f18e7e84125b264c21e
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-07-16 14:38:31 +0530

    UC8 Added

---
+++ EvenOddNumber.java
@@ -0,0 +1,16 @@
+package com.fifthdayassignment.program;
+
+public class Even_Odd_NumberUC8 {
+	public static void main(String[] args) { 
+	       int number=30;  // ## PUTTING VALUE TO CHECK THE NUMBER ##
+	       
+	       System.out.println("number is "+ number);
+	
+	    	   if (number%2==0) {
+	    		   System.out.println(" THE "+ number +"  IS THE EVEN NUMBER" );
+	    	   } 
+	    	   else 
+	    	   {   System.out.println(" THE "+ number +"  IS THE ODD NUMBER" );
+	       }
+		}
+}
